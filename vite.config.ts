cd1b2afbde27f9712d5391e5b4f1c3401b481f3d
Mootae1 <1728mootae@gmail.com>
2025-03-04 22:03:29 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Test/', // Replace with your actual repository name
+  base: '/MyMentalHealth/', // Replace with your actual repository name
 });
