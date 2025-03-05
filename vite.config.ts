b2a07a34bdb20f3fa5980d3286159aebed4fe576
mootae1 <1728mootae@gmail.com>
2025-03-05 10:33:14 +0700

    Fix path issues in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/MyMentalHealth/', // Replace with your actual repository name
-  base: '/MyMentalHealth/', // Replace with your actual repository name
 });
